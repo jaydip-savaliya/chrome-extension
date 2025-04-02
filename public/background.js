71e2818af5db6c9f74b01065b89f0e76a76e4892
jaydeep.savaliya <jaydeep.savaliya@aspiresoftserv.com>
2025-04-02 12:26:15 +0530

    update popup

--- public/background.js
+++ public/background.js
@@ -2,15 +2,43 @@ chrome.runtime.onInstalled.addListener(() => {
     console.log("Chrome Extension Installed");
 });
 
+let popupWindowId = null;
+
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message.action === "openPopup") {
-        chrome.windows.create({
+        if (popupWindowId) {
+            chrome.windows.get(popupWindowId, (window) => {
+                if (chrome.runtime.lastError || !window) {
+                    createPopup();
+                } else {
+                    chrome.windows.update(popupWindowId, { focused: true });
+                }
+            });
+        } else {
+            createPopup();
+        }
+    }
+});
+
+function createPopup() {
+    chrome.windows.create(
+        {
             url: chrome.runtime.getURL("index.html"),
             type: "popup",
             width: 400,
-            height: 500,
+            height: 600,
             top: 100,
             left: 100
-        });
+        },
+        (newWindow) => {
+            popupWindowId = newWindow.id;
+        }
+    );
+}
+
+chrome.windows.onRemoved.addListener((windowId) => {
+    if (windowId === popupWindowId) {
+        popupWindowId = null;
     }
 });
+
